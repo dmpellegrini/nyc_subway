6a3df5bc78648fba1242ca153d5ea95de8b91c77
Danny Pellegrini <danny.m.pellegrini@gmail.com>
2022-11-25 10:59:18 -0500

    Added port back to settings file

--- nyc_subway/settings.py
+++ nyc_subway/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': str(os.getenv('USER')),
         'PASSWORD': str(os.getenv('PASSWORD')),
         'HOST': str(os.getenv('HOST')),
-        # 'PORT': str(os.getenv('PORT')),
+        'PORT': str(os.getenv('PORT')),
     }
 }
 
